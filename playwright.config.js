608b6c766acdb83c137bd5e01bf35436a8e41b17
Ontor-Hazari <ontorhazary570@gmail.com>
2024-12-19 12:44:52 +0600

    update playwright config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,19 +4,19 @@ module.exports = {
     testDir: './tests',
     timeout: 200000,
     retries: 0, // Disable retries
-    reporter: [['html', { open: 'never' }]], // Generate HTML reports
+    reporter: [
+        ['html', { open: 'never' }], // Generate HTML reports
+        ['json', { outputFile: 'test-results.json' }] // Generate JSON report for GitHub Actions
+    ],
     use: {
         baseURL: process.env.BASE_URL, // Ensure BASE_URL is set
-        headless: true, // Set this to false if you want to view the browser during local testing
+        headless: true, // Set to false for local testing
         viewport: { width: 1280, height: 720 },
     },
-  
-    // Pattern for including test files
     testMatch: [
         '**/*.spec.js', // Include all spec files
-        '**/*.test.js'   // Include all test files
+        '**/*.test.js'  // Include all test files
     ],
-
     resolve: {
         alias: {
             '@page-objects': path.resolve(__dirname, './page-objects'),
