eaaf7f8f85e559cf09ce01b14e2bcf2646553df4
Ontor-Hazari <ontorhazary570@gmail.com>
2024-12-22 12:34:11 +0600

    headless mode select in false

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   use: {
     baseURL: process.env.BASE_URL, // Ensure BASE_URL is properly set in the environment
-    headless: false, // Set to false to view the browser during testing
+    headless: true, // Set to false to view the browser during testing
     viewport: { width: 1280, height: 720 }, // Browser viewport size
   },
   testMatch: [
