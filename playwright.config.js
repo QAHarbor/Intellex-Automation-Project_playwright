9d7bd8872f97b3fc76eb4f8812c4e97502d4081f
Ontor-Hazari <ontorhazary570@gmail.com>
2024-12-22 12:30:57 +0600

    Update yml file

--- playwright.config.js
+++ playwright.config.js
@@ -1,23 +1,24 @@
-require('@babel/register')({
-    extensions: ['.js', '.ts'],
-  });
-  
-  module.exports = {
-    testDir: './tests',
-    timeout: 200000,
-    retries: 0,
-    reporter: [
-      ['html', { open: 'never' }], // HTML report
-      ['json', { outputFile: 'test-results.json' }] // JSON report
-    ],
-    use: {
-        baseURL: process.env.BASE_URL, // Ensure BASE_URL is set
-        headless: false, // Set this to false if you want to view the browser during local testing
-        viewport: { width: 1280, height: 720 },
-    },
-    testMatch: [
-      '**/*.spec.js',
-      '**/*.test.js'
-    ],
-  };
-  
+module.exports = {
+  testDir: './tests',
+  timeout: 200000, // Timeout for each test in milliseconds
+  retries: 0, // Number of retries for failed tests
+  reporter: [
+    ['html', { open: 'never' }], // HTML report configuration
+    ['json', { outputFile: 'test-results.json' }] // JSON report configuration
+  ],
+  use: {
+    baseURL: process.env.BASE_URL, // Ensure BASE_URL is properly set in the environment
+    headless: false, // Set to false to view the browser during testing
+    viewport: { width: 1280, height: 720 }, // Browser viewport size
+  },
+  testMatch: [
+    '**/*.spec.js', // Match .spec.js test files
+    '**/*.test.js' // Match .test.js test files
+  ],
+  resolve: {
+    alias: {
+      '@page-objects': './page-objects',
+      '@fixtures': './tests/fixtures'
+    }
+  }
+};
